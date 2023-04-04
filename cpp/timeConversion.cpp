b68d448c41142d5b496c8433cb81981601b8fb11
locphp <locphp2394@gmail.com>
2023-04-04 19:58:10 +0700

    upload file

---
+++ cpp/timeConversion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string timeConversion(string s){
+    string hour_string = s.substr(0,2);
+    int hour = stoi(hour_string);
+    size_t pos = s.find("PM");
+    if(pos != string::npos){
+        if(hour < 12)
+            hour_string = to_string(hour+12);
+    }
+    else {
+        if(hour == 12)
+            hour_string = "00";
+    }  
+    return s.replace(0,2, hour_string).erase(s.size()-2);
+}
+
+
+
+int main(){
+    string s;
+    cin >> s;
+    cout << timeConversion(s)<< "\n";
+    return 0;
+}
